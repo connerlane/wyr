e94f468933e8cc9900dea0d0ed3259e6ec062c78
Conner Lane <seltzerfish@gmail.com>
2022-01-21 14:30:03 -0800

    asdasd

---
+++ Scenes/Player.gd
@@ -0,0 +1,32 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.handle_input()
+	
+func handle_input():
+	var run_speed = 8
+	if Input.is_key_pressed(KEY_D):
+		self.linear_velocity.x += run_speed
+	if Input.is_key_pressed(KEY_A):
+		self.linear_velocity.x -= run_speed
+	if Input.is_key_pressed(KEY_W):
+		self.linear_velocity.y -= run_speed
+	if Input.is_key_pressed(KEY_S):
+		self.linear_velocity.y += run_speed
+
+#func _input(event):
+#	if event is InputEventKey:
+#		if event.scancode == KEY_W:
+#		get_tree().quit()
