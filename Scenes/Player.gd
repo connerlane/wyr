6b60e7c55993623742f78b1b7d1a3605821c6360
Conner Lane <seltzerfish@gmail.com>
2022-01-21 17:04:35 -0800

    dddddd

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,12 +1,14 @@
-extends RigidBody2D
+extends Node2D
 
+export(PackedScene) var texas_avatar
 
 export var run_speed = 40
 export var max_run_speed = 260
 export var momentum_dampening = 0.94
 
 
-# Called when the node enters the scene tree for the first time.
+onready var body = get_node("SquareAvatar")
+
 func _ready():
 	pass # Replace with function body.
 
@@ -16,14 +18,24 @@ func _process(delta):
 
 func handle_input():
 	if Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_RIGHT):
-		self.linear_velocity.x += run_speed
+		body.linear_velocity.x += run_speed
 	if Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT):
-		self.linear_velocity.x -= run_speed
+		body.linear_velocity.x -= run_speed
 	if Input.is_key_pressed(KEY_W) or Input.is_key_pressed(KEY_UP):
-		self.linear_velocity.y -= run_speed
+		body.linear_velocity.y -= run_speed
 	if Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_DOWN):
-		self.linear_velocity.y += run_speed
-	if self.linear_velocity.length() > max_run_speed:
-		self.linear_velocity *= (max_run_speed/ self.linear_velocity.length())
-	self.linear_velocity *= momentum_dampening
+		body.linear_velocity.y += run_speed
+	if body.linear_velocity.length() > max_run_speed:
+		body.linear_velocity *= (max_run_speed/ body.linear_velocity.length())
+	body.linear_velocity *= momentum_dampening
 
+
+
+func _on_TexasTimer_timeout():
+	body = texas_avatar.instance()
+	add_child(body)
+	body.position = $SquareAvatar.position
+	body.angular_velocity = $SquareAvatar.angular_velocity
+	
+	remove_child($SquareAvatar)
+	
