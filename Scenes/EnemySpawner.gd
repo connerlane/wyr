f069b00b3bf42261f86547618bfb731fbecf96a7
Conner Lane <seltzerfish@gmail.com>
2022-01-22 19:34:26 -0800

    lll

--- Scenes/EnemySpawner.gd
+++ Scenes/EnemySpawner.gd
@@ -2,12 +2,17 @@ extends Node2D
 
 export(PackedScene) var enemy_scene
 signal wave_change(value)
-export var waves = 4
+var waves = 4
+export var default_waves = 4
+export var enemies_per_wave_start = 2
 export var enemies_per_wave = 2
 export var enemy_increase_per_wave = 3
+export var difficulty_ramp = 1.25
+
+var enemy_speed = 1
 
 var enemies = []
-
+var active = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -15,7 +20,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
+	if !active:
+		return
 	if self.get_children().size() >= enemies_per_wave:
 		$SpawnTimer.stop()
 	if self.get_children().size() <= 3 and $WaveTimer.is_stopped() and $SpawnTimer.is_stopped():
@@ -26,6 +32,7 @@ func begin_wave():
 
 func _on_SpawnTimer_timeout():
 	var t = enemy_scene.instance()
+	t.speed *= self.enemy_speed
 	var c = randi() % 4
 	if c == 0:
 		t.position.x = randi() % 800
@@ -44,11 +51,21 @@ func _on_SpawnTimer_timeout():
 func _on_WaveTimer_timeout():
 	$SpawnTimer.start()
 	self.waves -= 1
-	emit_signal("wave_change", self.waves)
 	self.enemies_per_wave += self.enemy_increase_per_wave
+	if self.waves == 0:
+		$WaveTimer.stop()
+		$SpawnTimer.stop()
+		active = false
+	emit_signal("wave_change", floor(self.waves))
 	
 func raise_difficulty():
-	pass
+	enemies_per_wave *= self.difficulty_ramp
+	enemies_per_wave_start *= 1.4
+	default_waves *= 1.1
+	self.enemy_speed *= self.difficulty_ramp
 	
 func reset():
-	pass
+	enemies_per_wave = enemies_per_wave_start
+	self.waves = self.default_waves
+	emit_signal("wave_change", floor(self.waves))
+	active = true
